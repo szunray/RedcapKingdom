6aff81cd0f5888daa400685633ee897e4f7dfa04
Jerry Belton <jerry.belton@gmail.com>
2018-10-13 00:19:20 -0700

    some free ground tiles temporarily

---
+++ app/src/main/java/com/example/savaque/redcapkingdom/Camera.java
@@ -0,0 +1,7 @@
+package com.example.savaque.redcapkingdom;
+
+public class Camera {
+    private int xPos;
+    private int yPos;
+    private int zoomLevel;
+}
---
+++ app/src/main/java/com/example/savaque/redcapkingdom/Level.java
@@ -0,0 +1,12 @@
+package com.example.savaque.redcapkingdom;
+
+// Hard code ALLTHETHINGS
+public class Level {
+
+    private Material[][] tileMap;
+
+    public class Material {
+        // Holds the information about a given material.
+        // These are interactable and on the same plane as the player.
+    }
+}
